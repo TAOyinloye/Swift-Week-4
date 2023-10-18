a0a2f0b64e15fe69f7e87be445887241246f210b
TAOyinloye <94188481+TAOyinloye@users.noreply.github.com>
2023-10-18 11:16:46 +0100

    Update Contents.swift

--- statics.playground/Contents.swift
+++ statics.playground/Contents.swift
@@ -14,6 +14,7 @@ class Student:Person {
 //  //  override static var count : Int {
 //        return 150
 //    }
+    //The static var above is commented because it will give an error.  static is not overrideable.  You can remove the comment to see that the compiler pops an reeor message.
     override  class var averageAge: Double {
         return 19.5
     }
